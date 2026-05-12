bc01c1740f043e417157cd5460885055866f3aa5
Gaurang12tyagi <126134404+Gaurang12tyagi@users.noreply.github.com>
2026-05-12 11:57:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
-
 module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true, // 👈 add this
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
